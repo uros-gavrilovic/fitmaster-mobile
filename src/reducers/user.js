ed91fb0b810b43a0da62d9731f0fa25be53f0364
Uros Gavrilovic <urosukigavrilovic@gmail.com>
2023-08-24 12:07:49 +0200

    Project skeleton implemented

---
+++ src/reducers/user.js
@@ -0,0 +1,49 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: undefined,
+  token: undefined,
+
+  loading: false,
+  error: undefined,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: initialState,
+
+  reducers: {
+    login(state, action) {
+      state.user = action.payload;
+      state.token = action.payload.token;
+
+      state.error = undefined;
+      state.loading = false;
+    },
+    logout(state) {
+      state.user = undefined;
+      state.token = undefined;
+
+      state.error = undefined;
+      state.loading = false;
+    },
+    updateUser(state, action) {
+      state.user = action.payload;
+
+      state.error = undefined;
+      state.loading = false;
+    },
+
+    actionStart(state) {
+      state.loading = true;
+      state.error = undefined;
+    },
+    actionError(state, action) {
+      state.error = action.payload;
+      state.loading = false;
+    },
+  },
+});
+
+export const userActions = userSlice.actions;
+export default userSlice.reducer;
