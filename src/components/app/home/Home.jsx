30d7ccdfc4383bc85d9ecb94c058b3932175c20b
Uros Gavrilovic <urosukigavrilovic@gmail.com>
2023-08-26 01:13:38 +0200

    Added notification snackbar

--- src/components/app/home/Home.jsx
+++ src/components/app/home/Home.jsx
@@ -1,13 +1,30 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Button } from "react-native-paper";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CustomAppBar from "../../reusable/menu/CustomAppBar";
+import CustomSnackbar from "../../reusable/notifications/CustomSnackbar";
 
 export default function Home() {
+  const [open, setOpen] = useState(false);
+
   return (
     <View>
       <CustomAppBar />
+      <CustomSnackbar
+        message={"Test Notification"}
+        icon="camera"
+        duration={3000} // 3 seconds
+        open={open}
+        setOpen={setOpen}
+      />
 
+      <Button
+        onPress={() => {
+          setOpen(true);
+        }}
+      >
+        CLICK ME
+      </Button>
       <Text variant="headlineMedium">Home!</Text>
     </View>
   );
